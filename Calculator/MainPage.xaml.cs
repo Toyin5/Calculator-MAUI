cfd28c9dfe9494700c499c1c4f6960cf5ceb2f11
ToyinM <toyinmuhammed50@gmail.com>
2024-01-10 09:23:52 +0100

    init

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            var buttonText = ((Button)sender).Text;
+            entry.Text += buttonText;
+        }
+
+        private void Submit(object sender, EventArgs e)
+        {
+            Solve();
+        }
+
+        private void Solve()
+        {
+            var text = entry.Text;
+            var ans = Convert.ToDecimal(new DataTable().Compute(text, null));
+            entry.Text = ans.ToString();
+
+        }
+
+        private void Delete(object sender, EventArgs e)
+        {
+            if (entry.Text.Length == 0)
+            {
+                return;
+            }
+            entry.Text = entry.Text.Substring(0, entry.Text.Length - 1);
+        }
+        private void OnEntryCompleted(object sender, EventArgs e)
+        {
+            Solve();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## Calculator App
+
+# Built with .NET MAUI
+
